164141f0716483b23a7d137dfd5c61c46aed82c4
Giulianogoliver <giulianogoliver84@gmail.com>
2026-07-25 02:27:24 -0300

    feat(ssm): refactor generate-report Lambda recipient email to use SSM Parameter Store /management_email dynamically

--- infra/terraform/ssm.tf
+++ infra/terraform/ssm.tf
@@ -9,3 +9,15 @@ resource "aws_ssm_parameter" "ses_source_email" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_ssm_parameter" "management_email" {
+  name  = "/feedback-management/${var.environment}/management_email"
+  type  = "String"
+  value = var.ses_source_email
+
+  tags = {
+    Environment = var.environment
+    Project     = "feedback-management"
+    ManagedBy   = "Terraform"
+  }
+}
